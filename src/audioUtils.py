031645189271eeb9e6a877c3a45c1e0c7d0f4e6e
mario <mariosilva@ua.pt>
2022-01-20 11:58:44 +0000

    added audio noise

--- src/audioUtils.py
+++ src/audioUtils.py
@@ -2,5 +2,5 @@ from consts import SAMPLE_MAX_FREQS
 import subprocess
 
 def add_noise(sample, sample_name, noise):
-    cmd = f"sox {sample} -p synth whitenoise vol {noise} | sox -m {sample} - {SAMPLE_MAX_FREQS} {sample_name}"
+    cmd = f"sox {sample} -p synth whitenoise vol {noise} | sox -m {sample} - {SAMPLE_MAX_FREQS}{sample_name}"
     subprocess.run([cmd], shell=True)
